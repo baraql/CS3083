df4c10a64eb164792b63233fb4445d4382451fed
Baraq Lipshitz <baraqlip@gmail.com>
2023-11-28 18:00:32 -0500

    Connected successfully

--- helloworld.php
+++ helloworld.php
@@ -1,1 +1,14 @@
-<?php echo "Hello World @^_^@" ?>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "Hello World @^_^@";
+
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=criminalthing", "root");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; // Display message upon successful connection
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage(); // Display error message if connection fails
+}
+?>

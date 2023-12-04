38a69274df49d944bce8fa7c318e9e9db10b772a
Baraq Lipshitz <baraqlip@gmail.com>
2023-12-03 19:18:14 -0500

    Add appeal working

--- addAppeals.php
+++ addAppeals.php
@@ -13,97 +13,102 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 $criminal_ID = array_key_exists("criminal_ID", $_GET) ? $_GET['criminal_ID'] : null;
 if ($criminal_ID == null) {
     header("Location: criminal.php");
-    exit(); 
+    exit();
 }
 
-$method = array_key_exists("m", $_REQUEST) ? $_REQUEST['m'] : null;
+// $method = array_key_exists("m", $_REQUEST) ? $_REQUEST['m'] : null;
+$method = 'a';
 $crime_ID = array_key_exists("crime_ID", $_REQUEST) ? $_REQUEST['crime_ID'] : null;
 
 $appealID = array_key_exists("appeal_ID", $_GET) ? $_GET['appeal_ID'] : null;
 $appeal = new Appeal;
-/* 
- 
-if ($method == 'a') {
-    add_appeal();
-} elseif ($method == 'e') {
-    $appeal = get_appeal_info_from_db();
-} elseif ($method == 'u') {
-    update_appeal();
-} elseif ($method == 'd') {
-    delete_appeal();
-}*/ 
+
+
+// if ($method == 'a') {
+//     add_appeal();
+// } elseif ($method == 'e') {
+//     $appeal = get_appeal_info_from_db();
+// } elseif ($method == 'u') {
+//     update_appeal();
+// } elseif ($method == 'd') {
+//     delete_appeal();
+// }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Appeals</title>
     <script>
-        function check(form) {
-            var appeal_ID = form['appeal_ID'];
-            var appeal_status = form['appeal_status'];
-            var filing_date = form['filing_date'];
-            var hearing_date = form['hearing_date'];
-            var result = false;
+    function check(form) {
+        var appeal_ID = form['appeal_ID'];
+        var appeal_status = form['appeal_status'];
+        var filing_date = form['filing_date'];
+        var hearing_date = form['hearing_date'];
+        var result = false;
 
-            if (isNaN(appeal_ID.value) || appeal_ID.value.trim().length < 5) {
-                alert('The Appeal ID must be a 5-length numeric value.');
-                appeal_ID.focus();
-            } else if (appeal_status.value.trim() == '') {
-                alert('The Appeal Status cannot be null.');
-                appeal_status.focus();
-            } else if (filing_date.value.trim() == '') {
-                alert('The Filing Date cannot be null.');
-                filing_date.focus();
-            } else if (hearing_date.value.trim() == '') {
-                alert('The Hearing Date cannot be null.');
-                hearing_date.focus();
-            } else {
-                result = true;
-            }
-
-            return result;
+        if (isNaN(appeal_ID.value) || appeal_ID.value.trim().length < 5) {
+            alert('The Appeal ID must be a 5-length numeric value.');
+            appeal_ID.focus();
+        } else if (appeal_status.value.trim() == '') {
+            alert('The Appeal Status cannot be null.');
+            appeal_status.focus();
+        } else if (filing_date.value.trim() == '') {
+            alert('The Filing Date cannot be null.');
+            filing_date.focus();
+        } else if (hearing_date.value.trim() == '') {
+            alert('The Hearing Date cannot be null.');
+            hearing_date.focus();
+        } else {
+            result = true;
         }
+
+        return result;
+    }
     </script>
 </head>
+
 <body>
 
-<form method="post" action="addAppeals.php" onsubmit="return check(this);">
-    <input type="hidden" name="m" value="<?php echo ($method == null) ? 'a' : 'u'; ?>"/>
-    <input type="hidden" name="criminal_ID" value="<?php echo $criminal_ID; ?>"/>
+    <form method="post" action="appeals_function.php" onsubmit="return check(this);">
+        <input type="hidden" name="m" value="<?php echo $method; ?>" />
+        <input type="hidden" name="criminal_ID" value="<?php echo $criminal_ID; ?>" />
 
-    <input type="hidden" name="m" value="<?php echo ($method == null) ? 'a' : 'u'; ?>"/>
-    <input type="hidden" name="crime_ID" value="<?php echo $crime_ID; ?>"/>
+        <input type="hidden" name="m" value="<?php echo $method; ?>" />
+        <input type="hidden" name="crime_ID" value="<?php echo $crime_ID; ?>" />
 
-    <label for="appeal_ID">Appeal ID:</label>
-    <input type="text" name="appeal_ID" maxlength="9" required><br>
+        <label for="appeal_ID">Appeal ID:</label>
+        <input type="text" name="appeal_ID" maxlength="9" required><br>
 
-    <label for="appeal_status">Appeal Status:</label>
-    <select id="appeal_status" name="appeal_status" required style="width: 100%; padding: 8px; margin-bottom: 16px; box-sizing: border-box;">
-        <option value="P">P</option>
-        <option value="C">A</option>
-    </select><br>
+        <label for="appeal_status">Appeal Status:</label>
+        <select id="appeal_status" name="appeal_status" required
+            style="width: 100%; padding: 8px; margin-bottom: 16px; box-sizing: border-box;">
+            <option value="P">P</option>
+            <option value="C">A</option>
+        </select><br>
 
-    <label for="filing_date">Filing Date:</label>
-    <input type="date" name="filing_date" required><br>
+        <label for="filing_date">Filing Date:</label>
+        <input type="date" name="filing_date" required><br>
 
-    <label for="hearing_date">Hearing Date:</label>
-    <input type="date" name="hearing_date"  required><br>
+        <label for="hearing_date">Hearing Date:</label>
+        <input type="date" name="hearing_date" required><br>
 
-    <button type="submit">Submit</button>
-    <button type="button">
-        <a href="popup.php?criminal_ID=<?php echo $criminal_ID; ?>" style="text-decoration: none; color: inherit;">Go Back</a>
-    </button>
-</form>
+        <button type="submit">Submit</button>
+        <button type="button">
+            <a href="popup.php?criminal_ID=<?php echo $criminal_ID; ?>"
+                style="text-decoration: none; color: inherit;">Go Back</a>
+        </button>
+    </form>
 
-<script>
+    <script>
     document.getElementById("appeal_status").value = "<?php echo $appeal->appeal_status; ?>";
-</script>
+    </script>
 
-<style>
+    <style>
     body {
         font-family: Arial, sans-serif;
         background-color: #f4f4f4;
@@ -155,7 +160,8 @@ if ($method == 'a') {
     .error-message {
         color: #f44336;
     }
-</style>
+    </style>
 
 </body>
+
 </html>
--- appeals_function.php
+++ appeals_function.php
@@ -1,14 +1,15 @@
 <?php
 include 'connect.php';
-
-class Appeal {
+class Appeal
+{
     public $appeal_ID;
     public $crime_ID;
     public $filing_date;
     public $hearing_date;
     public $appeal_status;
 
-    public static function fromArrayOrResult($arrOrRes) {
+    public static function fromArrayOrResult($arrOrRes)
+    {
         $appeal = new Appeal;
 
         $appeal->appeal_ID = $arrOrRes['appeal_ID'];
@@ -21,7 +22,8 @@ class Appeal {
     }
 }
 
-function get_appeal_info_from_db() {
+function get_appeal_info_from_db()
+{
     global $con;
 
     $crime_ID = array_key_exists('crime_ID', $_GET) ? $_GET['crime_ID'] : die("Crime ID required!");
@@ -36,7 +38,8 @@ function get_appeal_info_from_db() {
     return Appeal::fromArrayOrResult($row);
 }
 
-function add_appeal() {
+function add_appeal()
+{
     global $con;
     $criminal_ID = array_key_exists('criminal_ID', $_REQUEST) ? $_REQUEST['criminal_ID'] : die("Criminal ID required!");
 
@@ -52,14 +55,15 @@ function add_appeal() {
     try {
         $con->begin_transaction();
         $stmt = $con->prepare($sql);
-        $stmt->bind_param("iisss", 
-                        $appeal->appeal_ID, 
-                        $appeal->crime_ID, 
-                        $appeal->filing_date, 
-                        $appeal->hearing_date, 
-                        $appeal->appeal_status, 
-                        );
-                        
+        $stmt->bind_param(
+            "iisss",
+            $appeal_ID,
+            $crime_ID,
+            $filing_date,
+            $hearing_date,
+            $appeal_status
+        );
+
         $stmt->execute();
         $con->commit();
         header("location:popup.php?criminal_ID=$criminal_ID");
@@ -69,7 +73,8 @@ function add_appeal() {
     }
 }
 
-function update_appeal() {
+function update_appeal()
+{
     global $con;
     $criminal_ID = array_key_exists('criminal_ID', $_REQUEST) ? $_REQUEST['criminal_ID'] : die("Criminal ID required!");
 
@@ -80,13 +85,16 @@ function update_appeal() {
     try {
         $con->begin_transaction();
         $stmt = $con->prepare($sql);
-        $stmt->bind_param("iisss", 
-                        $appeal->appeal_ID, 
-                        $appeal->crime_ID, 
-                        $appeal->filing_date, 
-                        $appeal->hearing_date, 
-                        $appeal->appeal_status, 
-                        );
+        $stmt->bind_param(
+            "iisssi", // Matching types to placeholders
+            $appeal->appeal_ID,
+            $appeal->crime_ID,
+            $appeal->filing_date,
+            $appeal->hearing_date,
+            $appeal->appeal_status,
+            $appeal->appeal_ID // Make sure the WHERE clause ID is added here
+        );
+
 
         $stmt->execute();
         $con->commit();
@@ -97,7 +105,8 @@ function update_appeal() {
     }
 }
 
-function delete_appeal() {
+function delete_appeal()
+{
     global $con;
     $appeal_ID = array_key_exists('appeal_ID', $_POST) ? $_POST['appeal_ID'] : die("Appeal ID required!");
     $criminal_ID = array_key_exists('criminal_ID', $_GET) ? $_GET['criminal_ID'] : die("Criminal ID required!");
@@ -115,5 +124,22 @@ function delete_appeal() {
         $con->rollback();
         die($exception);
     }
+}
+
+
+if (isset($_POST['m'])) {
+    $method = $_POST['m'];
+} else {
+    return;
+}
+
+if ($method == 'a') {
+    add_appeal();
+} elseif ($method == 'e') {
+    $appeal = get_appeal_info_from_db();
+} elseif ($method == 'u') {
+    update_appeal();
+} elseif ($method == 'd') {
+    delete_appeal();
 }
 ?>
